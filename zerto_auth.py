09f1d5b84d8712923b2ba2030c92604f0a2a628f
Alex Schenck <linuxbeak@gmail.com>
2020-12-04 08:41:20 -0500

    Pre-release cleanup

--- zerto_auth.py
+++ zerto_auth.py
@@ -1,13 +1,18 @@
-import requests, urllib3
-from requests.auth import HTTPBasicAuth
-import json
-import zvm, vpg, vra
-#from secrets import zvm_ip, zvm_u, zvm_p, zca_u, zerto_license, zerto_tag
-import vmware.vapi.vsphere.client
-import time
+"""
+The zerto_auth.py module is the main authenticator script that must be run before any other API calls are accepted
+by the target Zerto Virtual Manager or Zerto Cloud Appliance.
+"""
 
+import requests
+from requests.auth import HTTPBasicAuth
 
 def login(zvm_ip, zvm_user, zvm_password):
+    """The login function returns a valid header, including an API token, that can then be passed to newly instansiated
+    classes from other modules.
+
+    The login function accepts three str-type inputs (IP addresss of ZVM/ZCA, username of ZVM/ZCA, and password of ZVM/
+    ZCA and returns a dict-type containing the valid headers for future API requests.
+    """
     sessionUrl = 'https://' + zvm_ip + ':9669/v1/session/add'
     print("Getting API token for " + zvm_ip + "...")
     auth_info = "{\r\n\t\"AuthenticationMethod\":1\r\n}"
@@ -27,106 +32,3 @@ def login(zvm_ip, zvm_user, zvm_password):
 
     else:
         print("HTTP %i - %s, Message %s" % (response.status_code, response.reason, response.text))
-
-'''
-def constructTagProtectedVpg(tagname):
-
-    protectedTag = tagname
-    vmlist = vsphere_client.vcenter.VM.list()
-    taglist = vsphere_client.tagging.Tag.list()
-    friendlyTagList = []
-    curatedTagList = []
-
-    for i in range(len(taglist)):
-        friendlyTagList.append(vsphere_client.tagging.Tag.get(taglist[i]))
-
-    for i in range(len(friendlyTagList)):
-        if friendlyTagList[i].name == protectedTag:
-            curatedTagList.append(friendlyTagList[i].id)
-
-    toBeProtectedVMs = vsphere_client.tagging.TagAssociation.list_attached_objects_on_tags(curatedTagList)
-
-    for i in range(len(toBeProtectedVMs[0].object_ids)):
-        print(toBeProtectedVMs[0].object_ids[i].id)
-        workingDict = {"VmIdentifier": str(vCenterUUID+'.' + toBeProtectedVMs[0].object_ids[i].id)}
-        workingOpen['Vms'].append(workingDict)
-
-    payload2 = json.dumps(workingOpen)
-    return payload2
-    #return v.createNewVpgSettingsObject(payload2).json()
-'''
-
-'''
-terraformWf = open('terraformvariables.json')  # Grab ZCA specific information from 'terraform output -json'
-terraformVars = json.load(terraformWf)
-terraformWf.close()
-ipOfZca = terraformVars.get('SecondZCAprivateIPaddress').get('value')
-pwOfZca = terraformVars.get('ZCADecrypted_Password').get('value')
-'''
-
-'''
-wf = open('testVpg.json')
-payload = json.dumps(json.load(wf))
-'''
-
-'''
-wf2 = open('automatedVpgSkel.json')
-workingOpen = json.load(wf2)
-wf2.close()
-'''
-
-'''
-vCenterUUID = '0ad85e47-6b7d-4a95-a60d-be3d79308223'
-
-session = requests.session()
-session.verify = False
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-vsphere_client = vmware.vapi.vsphere.client.create_vsphere_client(server="192.168.1.41", username=zvm_u, password=zvm_p, session=session)
-'''
-
-'''
-testVraDict = {
-                "DatastoreIdentifier":"0ad85e47-6b7d-4a95-a60d-be3d79308223.datastore-10",
-                "HostIdentifier":"0ad85e47-6b7d-4a95-a60d-be3d79308223.host-9",
-                "HostRootPassword":zvm_p,
-                "MemoryInGb":3,
-                "NumOfCpus":1,
-                "NetworkIdentifier":"0ad85e47-6b7d-4a95-a60d-be3d79308223.network-11",
-                "UsePublicKeyInsteadOfCredentials":False,
-                "PopulatePostInstallation":False,
-                "VraNetworkDataApi":{
-                    "DefaultGateway":"192.168.1.72",
-                    "SubnetMask":"255.255.255.0",
-                    "VraIPAddress":"192.168.1.80",
-                    "VraIPConfigurationTypeApi":"Static"
-                    }
-                }
-'''
-
-'''
-def multiPlatformDemo():
-
-    wf2 = open('automatedVpgSkel.json')
-    workingOpen = json.load(wf2)
-    wf2.close()
-    vczvmSession = login(zvm_ip, zvm_u, zvm_p)
-    awszcaSession = login(ipOfZca, zca_u, pwOfZca)
-    z = zvm.zvm(zvm_ip, vczvmSession)
-    z.addLicense(zerto_license)
-    v = vpg.vpgSettings(zvm_ip, vczvmSession)
-    zvmvra = vra.vra(zvm_ip, vczvmSession)
-    zvmvra.installVRA(json.dumps(testVraDict))
-    zca = zvm.zvm(ipOfZca, awszcaSession)
-    zca.addLicense(zerto_license)
-    zcaTokenObject = zca.generatePeeringToken()
-    zcaTokenActual = zcaTokenObject.json().get('Token')
-    testOutput = requests.post('https://' + zvm_ip +':9669/v1/peersites', headers=vczvmSession, data=json.dumps(
-                                {"HostName": ipOfZca, "Port":"9071", "Token":zcaTokenActual}), verify=False)
-    time.sleep(100)
-
-    workingOpen['Basic']['ProtectedSiteIdentifier'] = z.getLocalSiteInfo().json()['SiteIdentifier']
-    workingOpen['Basic']['RecoverySiteIdentifier'] = z.getLocalSiteInfo().json()['SiteIdentifier']
-    tagYoureIt=v.createNewVpgSettingsObject(constructTagProtectedVpg(zerto_tag)).json()
-    #tagYoureIt=json.loads(constructTagProtectedVpg(zerto_tag))
-    v.commitSettingsObject(tagYoureIt)
-'''
--- zvm.py
+++ zvm.py
@@ -1,13 +1,202 @@
+"""
+The zvm.py module contains properly-formatted API calls for various Zerto Virtual Manager (or ZCA Zerto Virtual Manager
+ service) tasks, including alert management, site pairing, site information gathering, and license management.
+"""
+
 import json
 import requests
 
+
 class zvm:
+    """
+    The zvm class houses ZVM-specific methods.
+
+    ...
+
+    Attributes
+    ----------
+    zvmip : str
+        the IP address of the target ZVM
+
+    headerwithkey : dict
+        a properly formatted dict containing the following key:value pairs:
+        {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            'x-zerto-session': str-type containing valid session key generated with zerto_auth.py
+        }
+
+    Methods
+    -------
+    infoAllAlerts(alertid=None)
+        Returns information on all alerts
+
+    infoSingleAlert(alertid)
+        Returns information on a single alert
+
+    validAlertEntitiesValues()
+        Returns information on valid alert entities
+
+    validAlertHelpIdentifiersValues()
+        Returns information on valid alert help identifiers
+
+    validAlertLevelValues()
+        Returns information on valid alert levels
+
+    dismissAlert(alertid)
+        Dismisses an active alert
+
+    undismissAlert(alertid)
+        "Undismisses" (makes active) a previously-acknowledged/dismissed alert
+
+    datastoreInfo()
+        Returns information on locally available datastores for use as a replication target.
+
+    datastoreSingleInfo(datastoreid)
+        Returns information on a specific datastore locally available for use as a replication target.
+
+    allEvents()
+        Returns information on all (recent) events stored by the ZVM.
+
+    filteredEvents()
+        UNDER CONSTRUCTION. Function not yet usable.
+
+    getSingleEvent(eventid)
+        Returns information on a single event currently stored by the ZVM.
+
+    getValidEventCategories()
+        Returns all valid event categories.
+
+    getValidEventEntities()
+        Returns valid event entities.
+
+    getValidEventTypes()
+        Returns valid event types.
+
+    addLicense(licensekey)
+        Accepts a str parameter containing a valid Zerto license key and applies it to the target ZVM or ZCA.
+
+    delLicense()
+        Removes the license currently applied to the target ZVM or ZCA.
+
+    getLicense()
+        Returns information on the license applied to the target ZVM or ZCA.
+
+    getLocalSiteInfo()
+        Returns local site information.
+
+    getValidPairingStatus()
+        Returns information on the pairing status of the target ZVM or ZCA to other sites (ie other ZVMs or ZCAs).
+
+    sendBillingData()
+        Sends billing data to Zerto Analytics. Meant for use by Zerto Cloud Provider partners.
+
+    getListOfPeerSites()
+        Returns information about all peer sites of the target ZVM/ZCA.
+
+    getSinglePeerSites(siteidentifier)
+        Returns information about a specific peer site of the target ZVM/ZCA.
+
+    getStatusOfPeerSites()
+        Returns information on the status of all peer sites of the target ZVM/ZCA.
+
+    generatePeeringToken()
+        Returns a peering token that can then be passed to a desired peer site for the sake of the peering handshake.
+
+    pairToSite()
+        UNDER CONSTRUCTION. Function not yet usable.
+
+    getRecoveryReport()
+        Returns a recovery report for the target ZVM/ZCA.
+
+    getResourcesReport()
+        Returns a resources report for the target ZVM/ZCA.
+
+    getAllTasks()
+        Returns all tasks for the recovery ZVM/ZCA.
+
+    getSingleTask(taskidentifier)
+        Returns information on a single specified task for the ZVM/ZCA.
+
+    getValidTaskTypes()
+        Returns information on valid task types.
+
+    getAllVirtualizationSites()
+        Returns information on all virtualization sites.
+
+    getSingleVirtualizationSite(siteidentifier)
+        Returns information on a single specified virtualization site.
+
+    getStorageClustersAtSite(siteidentifier)
+        Returns information about storage clusters at a specific site available to the ZVM.
+
+    getStorageAtSite(siteidentifier)
+        Returns information about all storage at the specified site available to the ZVM/ZCA.
+
+    getDevicesAtAllHosts(siteidentifier)
+        Returns information about all devices at all hosts at the specified site to the ZVM/ZCA.
+
+    getDevicesAtSingleHost(siteidentifier, hostidentifier)
+        Returns information about all devices at a single host at the specified site to the ZVM/ZCA.
+
+    getFoldersAtSite(siteidentifier)
+        Returns information about all folders at the specified site.
+
+    getHostClustersAtSite(siteidentifier)
+        Returns information about all host clusters at the specified site.
+
+    getHostsAtSite(siteidentifier)
+        Returns information about all hosts at the specified site.
+
+    getSingleHostAtSite(siteidentifier, hostidentifier)
+        Returns information about a single host at the specified site.
+
+    getNetworksAtSite(siteidentifier)
+        Returns information about all networks at the specified site.
+
+    getRepositoriesAtSite(siteidentifier)
+        Returns information about all LTR repositories at the specified site.
+
+    getResourcePoolsAtSite(siteidentifier)
+        Returns information about all resource pools at the specified site.
+
+    getUnprotectedVmsAtSite(siteidentifier)
+        Returns information about unprotected VMs at the specified site.
+
+    getVmInstanceTypes(siteidentifier)
+        Returns information about instance types at the specified site.
+
+    getAvailablevNets(siteidentifier)
+        Returns information about available Azure vNets at the specified site.
+
+    getAvailableSubnets(siteidentifier)
+        Returns information about available Azure subnets at the specified site.
+
+    getSecurityGroups(siteidentifier)
+        Returns information about available security groups at the specified site.
+
+    getAllVolumes()
+        Returns information about available volumes visible to the ZVM/ZCA.
+    """
 
     def __init__(self, zvmip, headerwithkey):
+        """
+        Parameters
+        ----------
+        zvmip : str
+            The IP of the ZVM or ZCA
+        headerwithkey : dict
+            A properly formatted dict containing the following key:value pairs:
+                {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                    'x-zerto-session': str-type containing valid session key generated with zerto_auth.py
+                }
+        """
         self.zvmurl = 'https://' + zvmip + ':9669/v1'
         self.headerwithkey = headerwithkey
 
-    def infoAllAlerts(self, alertid=None):
+    def infoAllAlerts(self):
 
         return requests.get(self.zvmurl + '/alerts', headers=self.headerwithkey, verify=False)
 
@@ -29,11 +218,13 @@ class zvm:
 
     def dismissAlert(self, alertid):
 
-        return requests.post(self.zvmurl + '/alerts/levels' + str(alertid) + '/dismiss', headers=self.headerwithkey, verify=False)
+        return requests.post(self.zvmurl + '/alerts/levels' + str(alertid) + '/dismiss', headers=self.headerwithkey,
+                             verify=False)
 
     def undismissAlert(self, alertid):
 
-        return requests.post(self.zvmurl + '/alerts/levels' + str(alertid) + '/undismiss', headers=self.headerwithkey, verify=False)
+        return requests.post(self.zvmurl + '/alerts/levels' + str(alertid) + '/undismiss', headers=self.headerwithkey,
+                             verify=False)
 
     def datastoreInfo(self):
 
@@ -46,8 +237,7 @@ class zvm:
     def allEvents(self):
         return requests.get(self.zvmurl + '/events', headers=self.headerwithkey, verify=False)
 
-
-    #def filteredEvents(self):
+    # def filteredEvents(self):
 
         # TODO: Create filteredEvents function
 
@@ -69,7 +259,8 @@ class zvm:
 
     def addLicense(self, licensekey):
 
-        return requests.put(self.zvmurl + '/license', data=json.dumps({"LicenseKey": licensekey}), headers=self.headerwithkey, verify=False)
+        return requests.put(self.zvmurl + '/license', data=json.dumps({"LicenseKey": licensekey}),
+                            headers=self.headerwithkey, verify=False)
 
     def delLicense(self):
 
@@ -85,11 +276,11 @@ class zvm:
 
     def getValidPairingStatus(self):
 
-        return requests.get(self.zvmurl + '/localsite/pairingstatuses', headers=testHeaders, verify=False)
+        return requests.get(self.zvmurl + '/localsite/pairingstatuses', headers=self.headerwithkey, verify=False)
 
     def sendBillingData(self):
 
-        return requests.post(self.zvmurl +'/localsite/billing/sendUsage', headers=testHeaders, verify=False)
+        return requests.post(self.zvmurl + '/localsite/billing/sendUsage', headers=self.headerwithkey, verify=False)
 
     def getListOfPeerSites(self):
 
@@ -107,12 +298,10 @@ class zvm:
 
         return requests.post(self.zvmurl + '/peersites/generatetoken', headers=self.headerwithkey, verify=False)
 
-    def pairToSite(self):
+    def pairToSite(self, peerip, pairingtoken):
         # TODO: Finish this module
-        response = requests.get(self.zvmurl + self.endPoint, headers=self.headerwithkey, verify=False)
-        print(response.text)
-        print(response)
-        return response
+        return requests.post(self.zvmurl + '/peersites', headers=self.headerwithkey, data=json.dumps(
+            {"HostName": peerip, "Port": "9071", "Token": pairingtoken}), verify=False)
 
     def getRecoveryReport(self):
 
@@ -140,73 +329,91 @@ class zvm:
 
     def getSingleVirtualizationSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier, headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier, headers=self.headerwithkey,
+                            verify=False)
 
     def getStorageClustersAtSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/datastoreclusters', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/datastoreclusters',
+                            headers=self.headerwithkey, verify=False)
 
     def getStorageAtSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/datastores', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/datastores',
+                            headers=self.headerwithkey, verify=False)
 
     def getDevicesAtAllHosts(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/devices', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/devices',
+                            headers=self.headerwithkey, verify=False)
 
     def getDevicesAtSingleHost(self, siteidentifier, hostidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/devices?hostIdentifier=' + hostidentifier, headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/devices?hostIdentifier=' +
+                            hostidentifier, headers=self.headerwithkey, verify=False)
 
-        # TODO: Verify this actually works, because the documentation contains what looks like a typo... -- Update -- It works. Doc bug...
+        # TODO: Verify this actually works, because the documentation contains what looks like a typo...
+        #  -- Update -- It works. Doc bug...
 
     def getFoldersAtSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/folders', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/folders',
+                            headers=self.headerwithkey, verify=False)
 
     def getHostClustersAtSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/hostclusters', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/hostclusters',
+                            headers=self.headerwithkey, verify=False)
 
     def getHostsAtSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/hosts', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/hosts',
+                            headers=self.headerwithkey, verify=False)
 
     def getSingleHostAtSite(self, siteidentifier, hostidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/hosts/' + hostidentifier, headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/hosts/' + hostidentifier,
+                            headers=self.headerwithkey, verify=False)
 
     def getNetworksAtSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/networks', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/networks',
+                            headers=self.headerwithkey, verify=False)
 
     def getRepositoriesAtSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/repositories', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/repositories',
+                            headers=self.headerwithkey, verify=False)
 
     def getResourcePoolsAtSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/resourcepools', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/resourcepools',
+                            headers=self.headerwithkey, verify=False)
 
     def getUnprotectedVmsAtSite(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/vms', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/vms', headers=self.headerwithkey,
+                            verify=False)
 
     def getVmInstanceTypes(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/publicCloud/vmInstanceTypes', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/publicCloud/vmInstanceTypes',
+                            headers=self.headerwithkey, verify=False)
 
     def getAvailablevNets(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/publicCloud/virtualNetworks', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/publicCloud/virtualNetworks',
+                            headers=self.headerwithkey, verify=False)
 
     def getAvailableSubnets(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/publicCloud/subnets', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/publicCloud/subnets',
+                            headers=self.headerwithkey, verify=False)
 
     def getSecurityGroups(self, siteidentifier):
 
-        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/publicCloud/securityGroups', headers=self.headerwithkey, verify=False)
+        return requests.get(self.zvmurl + '/virtualizationsites/' + siteidentifier + '/publicCloud/securityGroups',
+                            headers=self.headerwithkey, verify=False)
 
     def getAllVolumes(self):
 
